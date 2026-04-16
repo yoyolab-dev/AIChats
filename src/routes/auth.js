e29c7f3594a15035c16278a0d49506ee557bfce9
yoyolab-dev <dev@oujun.work>
2026-04-16 18:58:05 +0800

    debug(auth): add _debug/prisma route to troubleshoot

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -112,4 +112,14 @@ export default async function (fastify, opts) {
     // Here we just acknowledge
     return { success: true, data: { revoked: true } };
   });
+
+  // Debug: check prisma availability
+  fastify.get('/_debug/prisma', async (request, reply) => {
+    return {
+      hasFastifyPrisma: !!fastify.prisma,
+      hasRequestPrisma: !!request.prisma,
+      fastifyPrismaKeys: fastify.prisma ? Object.keys(fastify.prisma) : null,
+      requestPrismaKeys: request.prisma ? Object.keys(request.prisma) : null
+    };
+  });
 }
