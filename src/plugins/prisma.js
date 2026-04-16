d2249787c230fe6f658f1da84d5c7721c74d7da8
yoyolab-dev <dev@oujun.work>
2026-04-16 19:05:30 +0800

    debug(prisma): add log to verify plugin registration

--- src/plugins/prisma.js
+++ src/plugins/prisma.js
@@ -8,6 +8,7 @@ const prisma = new PrismaClient({
  * Fastify plugin to expose Prisma client via request.prisma and fastify.prisma.
  */
 export async function prismaPlugin(fastify, options) {
+  fastify.log.info('prismaPlugin registering');
   fastify.decorate('prisma', prisma);
   fastify.addHook('onRequest', async (request, reply) => {
     request.prisma = prisma;
