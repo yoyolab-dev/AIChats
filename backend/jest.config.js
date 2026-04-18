0adf23ffc39eb503565b28e5dc7858ec7a966516
yoyolab-dev <dev@oujun.work>
2026-04-19 00:40:07 +0800

    test(backend): run tests serially to avoid Prisma conflicts

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,9 +4,10 @@ export default {
     '^.+\\.js$': '@swc/jest',
   },
   moduleFileExtensions: ['js'],
-  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  testMatch: ['**/?(*.)+(spec|test).js'],
   collectCoverageFrom: ['src/**/*.js'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov'],
   verbose: true,
+  maxWorkers: 1, // Run tests serially to avoid Prisma connection conflicts
 };
